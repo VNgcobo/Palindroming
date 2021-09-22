e2103f9067b1ee587a40c843d9766abf4dadfcce
VNgcobo <85413379+VNgcobo@users.noreply.github.com>
2021-09-22 20:52:17 +0200

    Updates
    Rebuild of js; added eventListener to index page

--- js/site.js
+++ js/site.js
@@ -1,42 +1,38 @@
-//Get the string from the page
+//get the string from the page
 //controller function
 
 function getValue(){
-
-    //make sure that alert is invisible
+    //set the alert to invisible
     document.getElementById("alert").classList.add("invisible");
 
-    //get user string from page
+    //get the value from the page
     let userString = document.getElementById("userString").value;
-    
-    //check for palindrome
+
+    //check for palidrome
     let returnObj = checkForPalindrome(userString);
 
-    //display message to the screen
+    //display message to the page
     displayMessage(returnObj);
 }
 
-//check for a palindrome
 function checkForPalindrome(userString){
-
-    //convert to Lowercase
+    //convert string to lowercase
     userString = userString.toLowerCase();
 
-    //remove spaces and special characters
+    //remove special characters
     let regex = /[^a-z0-9]/gi;
     userString = userString.replace(regex,"");
 
     let revString = [];
     let returnObj = {};
 
-    for (let index = userString.length - 1; index >= 0; index--) {
-        revString += userString[index];      
+    for (let index = userString.length - 1; index >=0; index--){
+        revString += userString[index];
     }
 
-    if (revString == userString) {
+    if (revString == userString){
         returnObj.msg = "Well done! You've entered a palindrome!"
-    }
-    else{
+    } else{
         returnObj.msg = "Sorry! You did not enter a palindrome."
     }
 
@@ -46,9 +42,7 @@ function checkForPalindrome(userString){
 }
 
 function displayMessage(returnObj){
-
     document.getElementById("alertHeader").innerHTML = returnObj.msg;
     document.getElementById("msg").innerHTML = `Your phrase reversed is: ${returnObj.reversed}`;
     document.getElementById("alert").classList.remove("invisible");
-
 }
